092d9bd034e21c045c158da9af8c246b66fd3091
Zach Steele <zcsteele@gmail.com>
2018-11-29 04:57:08 +0000

    Add error handling to Environment singleton

--- src/main/java/Environment.java
+++ src/main/java/Environment.java
@@ -4,6 +4,10 @@ import java.util.concurrent.TimeUnit;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+
 public class Environment {
   private final static Environment self = new Environment();
   private ScheduledExecutorService threadManager =
@@ -27,5 +31,12 @@ public class Environment {
   private Runnable updateEnv = () -> {
     List<String> searchMatches = new ArrayList<String>();
     System.out.println("Running updateEnv() task");
+    try {
+      if (Files.notExists(Paths.get(Main.imageLogPrefix))) {
+        Files.createDirectory(Paths.get(Main.imageLogPrefix));
+      }
+    } catch (IOException error) {
+      System.err.println("WARNING: unable to read from image log");
+    }
   };
 }
